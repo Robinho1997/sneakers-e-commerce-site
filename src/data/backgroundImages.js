f9634944d5faf1f23e3bd9059f025c436f1ee946
Robinho1997 <robin.kempen@gmx.de>
2023-04-02 01:26:11 +0200

    added changing background section for the start page

---
+++ src/data/backgroundImages.js
@@ -0,0 +1,3 @@
+const backgroundImages = ["https://i.ibb.co/BqRm8Bz/artem-bondarchuk-XPBYi4-K8v-FI-unsplash-removebg-preview.png","https://i.ibb.co/7bQkrFQ/usama-akram-s-g-YAb-QTo-Xk-unsplash-removebg-preview.png" ,"https://i.ibb.co/mS6nVQp/sneaker1.png","https://i.ibb.co/5rFzTK4/sneaker2.png","https://i.ibb.co/v1gX4Dn/artem-bondarchuk-XPBYi4-K8v-FI-unsplash-removebg-preview.png"]
+
+export default backgroundImages
--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-// Produkt image welches das ganze bild zeigt
-// Logo und Name in der Mitte
-// Link welcher zum Shop führt
+import backgroundImages from "../data/backgroundImages";
+
+
 function Start() {
-    return ( 
-        <div className="startpage-welcome-div">
-            <h1 className="cloud-text cloud-title">Awesome </h1>
-            <h1 className="cloud-text cloud-title">Sneakers</h1>
-            <Link  to="/pages/shop">SHOP NOW</Link>
+    const [imageIndex, setImageIndex] = useState(0)
+    // change backgroundimage every second
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setImageIndex(prevIndex => (prevIndex + 1) % backgroundImages.length)
+        }, 500);
+        return () => clearInterval(interval)
+    }, [])
+
+    const styles = {
+        backgroundImage: "url(" + backgroundImages[imageIndex] + ")",
+        backgroundPosition: 'center',
+        backgroundSize: "1000px",
+        backgroundRepeat: 'no-repeat',
+      
+     
+    }
+
+    return (
+        <div>
+            <div className="startpage-welcome-div">
+                <h1 className="cloud-text cloud-title">Awesome</h1>
+                <h1 className="cloud-text cloud-title">Sneakers</h1>
+                <Link to="/pages/shop">SHOP NOW</Link>
+            </div>
+
+        <div className="startpage-sales-div">
+            <div style={styles}
+                className="changing-background-container">
+                <div className="sales-notice-card">
+                    <h1>Spare 40%-60%</h1>
+                    <Link to="/pages/shop">ANGEBOTE</Link>
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
